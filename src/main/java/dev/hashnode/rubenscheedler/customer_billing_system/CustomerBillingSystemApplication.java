89a044c01d8bd6ac6822eef7651cd28273ae3187
ruben <scheedler.r@tkppensioen.nl>
2022-12-02 14:46:33 +0100

    Add Spring Initializr code

---
+++ src/main/java/dev/hashnode/rubenscheedler/customer_billing_system/CustomerBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.customer_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBillingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hashnode/rubenscheedler/customer_billing_system/CustomerBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.customer_billing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
